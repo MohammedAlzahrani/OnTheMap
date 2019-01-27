e0e17b584722ca8c9e9c614e84e2aef3eff032e5
Mohammed ALZAHRANI <mody@mysystem.local>
2019-01-27 09:31:05 +0300

    added the login ui elements

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Mohammed ALZAHRANI on 1/18/19.
+//  Copyright © 2019 Mohammed ALZAHRANI. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var debugLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty{
+            debugLable.text = "email or password field is empty"
+        } else{
+            print("login")
+        }
+    }
+
+}
+
---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Mohammed ALZAHRANI on 1/18/19.
+//  Copyright © 2019 Mohammed ALZAHRANI. All rights reserved.
+//
+
+import Foundation
